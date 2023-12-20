8535174fd83e8d909d74fbcec2a55ff606d6391d
Jasmine <triki.yasmine@esprit.tn>
2023-12-20 13:52:56 +0100

    version1

---
+++ src/edu/connexion/tools/MyConnection.java
@@ -0,0 +1,7 @@
+package edu.connexion.tools;
+
+public class MyConnection {
+    private String url="jdbc:mysql://localhost:3306/WomenEmpowerment";
+    private String login="root";
+    private String pwd="";
+}
